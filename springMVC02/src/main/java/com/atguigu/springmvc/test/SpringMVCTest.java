4e13c7339347972d671dcc71237be9ea940d64e6
cyhbyw <swust2009cyh@qq.com>
2018-01-07 22:08:10 +0800

    initial import: HttpMessageConverter related

--- springMVC02/src/main/java/com/atguigu/springmvc/test/SpringMVCTest.java
+++ springMVC02/src/main/java/com/atguigu/springmvc/test/SpringMVCTest.java
@@ -1,9 +1,22 @@
 package com.atguigu.springmvc.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.atguigu.springmvc.crud.dao.EmployeeDao;
 import com.atguigu.springmvc.crud.entities.Employee;
@@ -20,6 +33,36 @@ public class SpringMVCTest {
         System.out.println("save: " + employee);
         employeeDao.save(employee);
         return "redirect:/emps";
+    }
+
+    @ResponseBody
+    @RequestMapping("/testJson")
+    public Collection<Employee> testJson() {
+        System.out.println("testJson");
+        return employeeDao.getAll();
+    }
+
+    @ResponseBody
+    @RequestMapping("/testHttpMessageConverter")
+    public String testHttpMessageConverter(@RequestBody String body) {
+        System.out.println(body);
+        return "hello world! " + new Date();
+    }
+
+    @RequestMapping("/testResponseEntity")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+        ServletContext servletContext = session.getServletContext();
+        InputStream in = servletContext.getResourceAsStream("/files/abc.txt");
+        byte[] body = new byte[in.available()];
+        in.read(body);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment;filename=abc.txt");
+
+        HttpStatus statusCode = HttpStatus.OK;
+
+        ResponseEntity<byte[]> response = new ResponseEntity<>(body, headers, statusCode);
+        return response;
     }
 
 }
--- springMVC02/src/main/webapp/index.jsp
+++ springMVC02/src/main/webapp/index.jsp
@@ -2,11 +2,41 @@
 <html>
 <head>
     <title>CYH</title>
+    <script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#testJson").click(function () {
+                var url = this.href;
+                var args = {};
+                $.post(url, args, function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var id = data[i].id;
+                        var lastName = data[i].lastName;
+
+                        alert(id + ": " + lastName);
+                    }
+                });
+                return false;
+            });
+        })
+    </script>
 </head>
 <body>
 
 <a href="emps">List All Employees</a>
 
+<br><br>
+<a href="testJson" id="testJson">Test Json</a>
+
+<br><br>
+<form action="testHttpMessageConverter" method="POST" enctype="multipart/form-data">
+    File: <input type="file" name="file"/>
+    Desc: <input type="text" name="desc"/>
+    <input type="submit" value="Submit"/>
+</form>
+
+<br><br>
+<a href="testResponseEntity">Test ResponseEntity</a>
 
 </body>
 </html>
