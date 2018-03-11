608d556e6b44d59aed6775d31ea12adfca22ed83
cyhbyw <swust2009cyh@qq.com>
2018-03-11 18:19:07 +0800

    initial import: spring mvc work flow, DEBUG purpose

---
+++ springMVC03/src/main/java/com/atguigu/springmvc/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.atguigu.springmvc;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+
+
+
+    @RequestMapping("/helloworld")
+    public String hello() {
+        System.out.println("Hello world: " + new Date());
+        return "success";
+    }
+
+}
